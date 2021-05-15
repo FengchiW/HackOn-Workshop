b1d89305fdb2099e6d39c0d72ff6d1385aab4e11
Wilson (Fengchi) Wang <wilsonfwang@gmail.com>
2021-05-14 21:53:06 -0400

    back end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
 
+const low = require("lowdb");
+const FileSync = require('lowdb/adapters/FileSync')
+
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+
+db.defaults({ posts: []}).write()
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  var values = db.get('posts', []).value()
+
+  res.render('index', { title: 'Covid Tips', posts: values});
 });
+
+router.post('/', function(req, res) {
+  var fname = req.body.fname
+
+  db.get('posts')
+      .push({ id: 1, post: fname})
+      .write()
+
+  var values = db.get('posts', []).value()
+
+  res.render('index', {title: 'Covid Tips', posts: values})
+})
+
+router.get('/api/data', () => {
+  res.send(db.get('posts', []).value())
+})
 
 module.exports = router;
